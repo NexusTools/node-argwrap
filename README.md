1665bcaaa66f0659bb57176e24296db7449132ff
Katelyn Slater <ktaeyln@gmail.com>
2014-11-27 12:51:54 -0500

    Initial commit, separated from node-netgate

---
+++ README.md
@@ -0,0 +1,16 @@
+[![Package Version](https://img.shields.io/npm/v/argwrap.svg)](https://www.npmjs.org/package/argwrap) [![Build Status](https://travis-ci.org/NexusTools/node-argwrap.svg)](https://travis-ci.org/NexusTools/node-argwrap) [![Coverage Status](https://img.shields.io/coveralls/NexusTools/node-argwrap.svg)](https://coveralls.io/r/NexusTools/node-argwrap?branch=master) [![Apache License 2.0](http://img.shields.io/hexpm/l/plug.svg)](http://www.apache.org/licenses/LICENSE-2.0.html) [![Gratipay Tips](https://img.shields.io/gratipay/NexusTools.svg)](https://gratipay.com/NexusTools/)
+
+node-argwrap
+------------
+Scans a given argument and detects its arguments, than maps those onto an object through a wrapper function which can be called whenever.
+Makes it easy to provide plugins that have a dynamic order or set of arguments with multiple names.
+
+See [the testing source](https://raw.githubusercontent.com/NexusTools/node-argwrap/master/test/tests.js) for how to use this library.
+
+```
+npm install -g argwrap
+```
+
+legal
+-----
+node-argwrap is licensed under [Apache License 2.0](LICENSE.md)
---
+++ index.js
@@ -0,0 +1,75 @@
+/*
+    Copied from http://stackoverflow.com/questions/1007981/how-to-get-function-parameter-names-values-dynamically-from-javascript#answer-9924463
+*/
+
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var ARGUMENT_NAMES = /([^\s,]+)/g;
+function argnames(func) {
+	var fnStr = func.toString().replace(STRIP_COMMENTS, '')
+	var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES)
+	if(result === null)
+		throw new Error("Failed to parse arguments from function source...");
+	return result
+}
+
+var $break = new Object();
+var noescape = /^[a-z_\$][a-z_\-0-9\$]*$/i;
+function argWrap0(func, required, friendlyname) {
+    var names = argnames(func);
+    var revNames = names.slice(0).reverse();
+    var source = "(function " + (friendlyname || func.name || "anonymous") + "_argwrap_mapper(argmap){";
+
+    var _else = false;
+    var until = revNames.length;
+    try {
+        if(until > 0) {
+            revNames.forEach(function(name) {
+                var isConstant = required && required.indexOf(name) != -1;
+
+                if(!isConstant) {
+                    if(_else)
+                        source += "}else ";
+                    else
+                        _else = true;
+
+                    source += "if(" + JSON.stringify(name) + " in argmap){";
+                } else if(_else)
+                    source += "}else{";
+                source += "return [";
+                
+                var _and = false;
+                for(var i=0;i<until;i++) {
+                    if(_and)
+                        source += ",";
+                    else
+                        _and = true;
+                    source += "argmap" + "." + names[i];
+                }
+                source += "];";
+                if(isConstant)
+                    throw $break;
+                until--;
+            });
+            source += "}else{";
+        }
+        source += "return [];";
+    } catch(e) {
+        if(e !== $break)
+            throw e;
+    }
+    if(_else)
+        source += "}";
+    source += "})";
+    
+    var mapper = eval(source);
+    return [function(arguments) {
+        return func.apply(func, mapper(arguments));
+    }, names];
+}
+function argWrap(func, required, friendlyname) {
+    return argWrap0(func, required, friendlyname)[0];
+}
+
+module.exports = argWrap;
+module.exports.wrap0 = argWrap0;
+module.exports.names = argnames;
